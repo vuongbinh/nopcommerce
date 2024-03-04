c8d74c2884da815b59fa4e1c79745e7e4ab7752f
Vuong Binh <binh.vuonghao@gmail.com>
2024-03-04 14:50:43 +0700

    Update test case register a user

--- src/test/java/framework/objects/personal.java
+++ src/test/java/framework/objects/personal.java
@@ -1,15 +1,23 @@
 package framework.objects;
 
+import com.github.javafaker.Faker;
+
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class personal {
     private String lastName;
     private String firstName;
-    private Date DOB;
+    private String DOB;
     private String email;
-
-    public personal(String lastName, String firstName, Date DOB, String email) {
+    public Faker faker = new Faker();
+    public personal(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM-dd-yyyy");
+        this.lastName = faker.name().lastName();
+        this.firstName = faker.name().firstName();
+        this.DOB = simpleDateFormat.format(faker.date().birthday(18,95));
+        this.email = faker.internet().safeEmailAddress();
+    }
+    public personal(String lastName, String firstName, String DOB, String email) {
         this.lastName = lastName;
         this.firstName = firstName;
         this.DOB = DOB;
@@ -25,8 +33,7 @@ public class personal {
     }
 
     public String getDOB() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM-dd-yyyy");
-        return simpleDateFormat.format(this.DOB);
+        return DOB;
     }
 
     public String getEmail() {
--- src/test/java/framework/pages/register.java
+++ src/test/java/framework/pages/register.java
@@ -1,16 +1,30 @@
 package framework.pages;
 
-import com.github.javafaker.Faker;
 import framework.objects.customer;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Date;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Properties;
+import java.util.Random;
 
 public class register extends pageController {
-    Faker faker = new Faker();
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM-dd-yyyy");
     String registerURL = baseURL.concat("/register");
-
-
+    {
+        try {
+            InputStream input = new FileInputStream("src/test/java/framework/supporter/resources/locators.properties");
+            pros.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public register(WebDriver driver) {
         super(driver);
     }
@@ -18,5 +32,53 @@ public class register extends pageController {
     @Override
     public void open() {
         driver.get(registerURL);
+        new WebDriverWait(driver, Duration.ofSeconds(60)).until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+    }
+
+    public void fillFormRandom() {
+        customer cus = new customer();
+
+        By selGenderMale = By.id(pros.getProperty("id_selGenderMale"));
+        By selGenderFemale = By.id(pros.getProperty("id_selGenderFemale"));
+        By tbxFirstname = By.id(pros.getProperty("id_tbxFirstname"));
+        By tbxLastname = By.id(pros.getProperty("id_tbxLastname"));
+        By drdDOB = By.name(pros.getProperty("name_drdDOB"));
+        By drdMOB = By.name(pros.getProperty("name_drdMOB"));
+        By drdYOB = By.name(pros.getProperty("name_drdYOB"));
+        By tbxCompany = By.id(pros.getProperty("id_tbxCompanyname"));
+        By tbxEmail = By.id(pros.getProperty("id_tbxEmail"));
+        By cbxNewlestter = By.id(pros.getProperty("id_cbxNewsletter"));
+        By tbxPassword = By.id(pros.getProperty("id_tbxPassword"));
+        By tbxConfirmpassword = By.id(pros.getProperty("id_tbxConfirmpassword"));
+
+        Random rand = new Random();
+        boolean isMale = rand.nextBoolean();
+        if (isMale) {
+            driver.findElement(selGenderMale).click();
+        } else {
+            driver.findElement(selGenderFemale).click();
+        }
+        driver.findElement(tbxFirstname).sendKeys(cus.getFirstName());
+        driver.findElement(tbxLastname).sendKeys(cus.getLastName());
+
+        String arr[] = cus.getDOB().split("-");
+        driver.findElement(drdDOB).sendKeys(arr[1]);
+        driver.findElement(drdMOB).sendKeys(arr[0]);
+        driver.findElement(drdYOB).sendKeys(arr[2]);
+
+        driver.findElement(tbxEmail).sendKeys(cus.getEmail());
+        driver.findElement(tbxCompany).sendKeys(cus.getCompanyName());
+
+        if (!driver.findElement(cbxNewlestter).isSelected()) {
+            driver.findElement(cbxNewlestter).click();
+        }
+
+        driver.findElement(tbxPassword).sendKeys(cus.getPassword());
+        driver.findElement(tbxConfirmpassword).sendKeys(cus.getPassword());
+    }
+
+    public void submitForm() {
+        By btnRegister = By.id(pros.getProperty("id_btnRegister"));
+        driver.findElement(btnRegister).click();
     }
 }
